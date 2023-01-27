b84f36a71f649dfadc7fb8fbdcdf3824ffa65368
Umarbek Nasimov <umarbeknasimov@gmail.com>
2023-01-27 17:01:27 -0500

    exponential steps converted to obj

--- constants.py
+++ constants.py
@@ -1,5 +1,6 @@
 import torch
+from foundations.step import Step
 
 USER_DIR = '/om/user/unasimov'
-EXPONENTIAL_STEPS = [2**i for i in range(20)]
+EXPONENTIAL_STEPS = [Step.from_iteration(2**i) for i in range(20)]
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
