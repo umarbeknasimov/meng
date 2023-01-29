38c01b4a85ac1d237328862d9b07fbe0bae4231f
Umarbek Nasimov <umarbeknasimov@gmail.com>
2023-01-29 00:29:49 -0500

    changing file dir

--- foundations/paths.py
+++ foundations/paths.py
@@ -9,5 +9,5 @@ def state_dict(root, step): return os.path.join(root, f'ep{step.ep}_it{step.it}'
 def train(root, seed, init_step=None, init_step_seed=None):
     train_details = f's_{seed}'
     if init_step and init_step_seed:
-        train_details += f'__init_ep{init_step.ep}_it{init_step.it}_s{init_step_seed}'
+        train_details = os.path.join(f'init_ep{init_step.ep}_it{init_step.it}_s{init_step_seed}', train_details)
     return os.path.join(root, 'train', train_details)
