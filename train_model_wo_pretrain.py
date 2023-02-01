776e5ac5e33361d17ff315a0a9eabdf14272af86
Umarbek Nasimov <umarbeknasimov@gmail.com>
2023-02-01 00:20:15 -0500

    making data order seed 8 digits

--- train_model_wo_pretrain.py
+++ train_model_wo_pretrain.py
@@ -5,7 +5,7 @@ from training.desc import TrainingDesc
 from training.runner import TrainingRunner
 
 def main():
-    training_hparams = TrainingHparams(data_order_seed=torch.randint(12, (1,)).item())
+    training_hparams = TrainingHparams(data_order_seed=torch.randint(1e8, (1,)).item())
     dataset_hparams = DatasetHparams()
     training_desc = TrainingDesc(dataset_hparams=dataset_hparams, training_hparams=training_hparams)
     runner = TrainingRunner(training_desc=training_desc)
