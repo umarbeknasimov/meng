006f1a8437fda857d05c95b06ae8f71b8030599f
Umarbek Nasimov <umarbeknasimov@gmail.com>
2023-04-18 14:51:49 -0400

    adding saved steps property to training description

--- training/desc.py
+++ training/desc.py
@@ -1,6 +1,7 @@
 import argparse
 from dataclasses import dataclass
 import os
+import datasets.registry
 
 
 from environment import environment
@@ -34,3 +35,12 @@ class TrainingDesc(desc.Desc):
             environment.get_user_dir(),
             experiment, 
             self.hashname))
+
+    def _train_dataset_steps_dense(self):
+        iterations_per_epoch = datasets.registry.get(self.dataset_hparams).iterations_per_epoch
+        end_step = [Step.from_str(self.training_hparams.training_steps, iterations_per_epoch)]
+        return [Step.zero(iterations_per_epoch)] + Step.get_log_2_steps_dense(end_step) + [end_step]
+
+    @property
+    def saved_steps(self):
+        return self._train_dataset_steps_dense()
